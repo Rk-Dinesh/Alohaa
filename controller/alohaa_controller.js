921430d370f7c9a9ed2e279cbe3aedc0c8e1d477
Rk-Dinesh <rdkumar881@gmail.com>
2025-01-27 20:50:15 +0530

    commit

--- controller/alohaa_controller.js
+++ controller/alohaa_controller.js
@@ -1,5 +1,8 @@
 const AlohaaService = require('../service/alohaa_service')
 exports.createAlohaa = async (req, res, next) => {
+    const event = req.body;
+    console.log(event);
+    
     try {
         const { organisation_id, call_type,caller_number, receiver_number,did_number,received_at,ended_at,call_id,call_status,call_duration,call_recording_url,agent_name } = req.body;
         const alohaa = await AlohaaService.createAlohaa({organisation_id, call_type,caller_number, receiver_number,did_number,received_at,ended_at,call_id,call_status,call_duration,call_recording_url,agent_name});
